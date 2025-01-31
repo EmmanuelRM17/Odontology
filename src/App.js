4807c0a2eb42a59623d2eb913514a5f114948522
EmmanuelRM17 <emma041117@gmail.com>
2025-01-31 01:36:29 -0600

    new

--- src/App.js
+++ src/App.js
@@ -1,44 +1,65 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
-import PrivateRoute from './components/Tools/PrivateRoute';
-import ErrorPage from './components/Tools/ErrorPage.jsx';
+import React, { useEffect, useState, useRef } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import PrivateRoute from "./components/Tools/PrivateRoute";
+import ErrorPage from "./components/Tools/ErrorPage.jsx";
 
 // Componentes Importados
-import Home from './pages/_home/pages/Home';
-import Register from './pages/_home/pages/Register';
-import Login from './pages/_home/pages/Login';
-import LayoutConEncabezado from './components/Layout/LayoutConEncabezado';
-import Recuperacion from './pages/_home/pages/Recuperacion';
-import Reset from './pages/_home/pages/CambiarContrasena';
-import Preguntas from './pages/_home/pages/Preguntas';
-import Contactanos from './pages/_home/pages/Contactanos';
-import Agendar from './pages/_home/pages/Agendar';
-import Acerca from './pages/_home/pages/AcerdaDe';
+import Home from "./pages/_home/pages/Home";
+import Register from "./pages/_home/pages/Register";
+import Login from "./pages/_home/pages/Login";
+import LayoutConEncabezado from "./components/Layout/LayoutConEncabezado";
+import Recuperacion from "./pages/_home/pages/Recuperacion";
+import Reset from "./pages/_home/pages/CambiarContrasena";
+import Preguntas from "./pages/_home/pages/Preguntas";
+import Contactanos from "./pages/_home/pages/Contactanos";
+import Agendar from "./pages/_home/pages/Agendar";
+import Acerca from "./pages/_home/pages/AcerdaDe";
 
 // Rutas del paciente
-import Principal from './pages/paciente/pages/Principal';
-import LayoutPaciente from './pages/paciente/compartidos/LayoutPaciente';
-import Perfil from './pages/paciente/pages/Perfil';
+import Principal from "./pages/paciente/pages/Principal";
+import LayoutPaciente from "./pages/paciente/compartidos/LayoutPaciente";
+import Perfil from "./pages/paciente/pages/Perfil";
 
 // Rutas del administrador
-import LayoutAdmin from './pages/administrador/assets/compartidos/LayoutAdmin';
-import PrincipalAdmin from './pages/administrador/pages/Principal';
-import Configuracion from './pages/administrador/pages/Configuracion';
-import Reportes from './pages/administrador/pages/reportes';
-import PerfilEmpresa from './pages/administrador/pages/PerfilEmpresa';
-import Pacientes from './pages/administrador/pages/PatientsReport.jsx';
+import LayoutAdmin from "./pages/administrador/assets/compartidos/LayoutAdmin";
+import PrincipalAdmin from "./pages/administrador/pages/Principal";
+import Configuracion from "./pages/administrador/pages/Configuracion";
+import Reportes from "./pages/administrador/pages/reportes";
+import PerfilEmpresa from "./pages/administrador/pages/PerfilEmpresa";
+import Pacientes from "./pages/administrador/pages/PatientsReport.jsx";
 
 function App() {
-  const [tituloPagina, setTituloPagina] = useState('_');
-  const [logo, setLogo] = useState('');
+  const [tituloPagina, setTituloPagina] = useState("_");
+  const [logo, setLogo] = useState("");
   const [fetchErrors, setFetchErrors] = useState(0);
   const [loading, setLoading] = useState(true);
-  const intervalRef = useRef(null); // Referencia para el intervalo
+  const intervalRef = useRef(null);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => {
+      window.location.reload();
+    };
+
+    const handleOffline = () => {
+      setIsOnline(false);
+    };
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
 
   const fetchTitleAndLogo = async (retries = 3) => {
     try {
-      const response = await axios.get('http://localhost:3001/api/perfilEmpresa/getTitleAndLogo');
+      const response = await axios.get(
+        "http://localhost:3001/api/perfilEmpresa/getTitleAndLogo"
+      );
       const { nombre_empresa, logo } = response.data;
 
       if (nombre_empresa) {
@@ -46,11 +67,13 @@ function App() {
         setTituloPagina(nombre_empresa);
       }
       if (logo) {
-        const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-        link.type = 'image/x-icon';
-        link.rel = 'shortcut icon';
+        const link =
+          document.querySelector("link[rel*='icon']") ||
+          document.createElement("link");
+        link.type = "image/x-icon";
+        link.rel = "shortcut icon";
         link.href = `data:image/png;base64,${logo}`;
-        document.getElementsByTagName('head')[0].appendChild(link);
+        document.getElementsByTagName("head")[0].appendChild(link);
         setLogo(`data:image/png;base64,${logo}`);
       }
 
@@ -77,16 +100,18 @@ function App() {
 
     return () => {
       if (intervalRef.current) {
-        clearInterval(intervalRef.current); 
+        clearInterval(intervalRef.current);
       }
     };
-  }, []); 
+  }, []);
 
   useEffect(() => {
     if (fetchErrors >= 5) {
-      console.error("Demasiados errores al intentar conectarse con el backend.");
+      console.error(
+        "Demasiados errores al intentar conectarse con el backend."
+      );
       if (intervalRef.current) {
-        clearInterval(intervalRef.current); 
+        clearInterval(intervalRef.current);
       }
     }
   }, [fetchErrors]);
@@ -97,34 +122,148 @@ function App() {
     }
   }, [loading]);
 
+  if (!isOnline) {
+    return (
+      <Router>
+        <ErrorPage
+          errorCode={502}
+          errorMessage="Sin conexión a Internet"
+          detailedMessage="La página se actualizará automáticamente cuando se restablezca la conexión."
+        />
+      </Router>
+    );
+  }
+
   return (
     <Router>
       <Routes>
         {/* Rutas públicas */}
-        <Route path="/" element={<LayoutConEncabezado><Home /><Preguntas /></LayoutConEncabezado>} />
-        <Route path="/FAQ" element={<LayoutConEncabezado><Preguntas /></LayoutConEncabezado>} />
-        <Route path="/Contact" element={<LayoutConEncabezado><Contactanos /></LayoutConEncabezado>} />
-        <Route path="/register" element={<LayoutConEncabezado><Register /></LayoutConEncabezado>} />
+        <Route
+          path="/"
+          element={
+            <LayoutConEncabezado>
+              <Home />
+              <Preguntas />
+            </LayoutConEncabezado>
+          }
+        />
+        <Route
+          path="/FAQ"
+          element={
+            <LayoutConEncabezado>
+              <Preguntas />
+            </LayoutConEncabezado>
+          }
+        />
+        <Route
+          path="/Contact"
+          element={
+            <LayoutConEncabezado>
+              <Contactanos />
+            </LayoutConEncabezado>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <LayoutConEncabezado>
+              <Register />
+            </LayoutConEncabezado>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/agendar-cita" element={<Agendar />} />
-        <Route path="/about" element={<LayoutConEncabezado><Acerca /></LayoutConEncabezado>} />
+        <Route
+          path="/about"
+          element={
+            <LayoutConEncabezado>
+              <Acerca />
+            </LayoutConEncabezado>
+          }
+        />
         <Route path="/recuperacion" element={<Recuperacion />} />
         <Route path="/resetContra" element={<Reset />} />
 
         {/* Rutas protegidas del paciente */}
-        <Route path="/Paciente/principal" element={<PrivateRoute><LayoutPaciente><Principal /></LayoutPaciente></PrivateRoute>} />
-        <Route path="/Paciente/perfil" element={<PrivateRoute><Perfil /></PrivateRoute>} />
+        <Route
+          path="/Paciente/principal"
+          element={
+            <PrivateRoute>
+              <LayoutPaciente>
+                <Principal />
+              </LayoutPaciente>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/Paciente/perfil"
+          element={
+            <PrivateRoute>
+              <Perfil />
+            </PrivateRoute>
+          }
+        />
 
         {/* Rutas protegidas del administrador */}
-        <Route path="/Administrador/principal" element={<PrivateRoute><LayoutAdmin><PrincipalAdmin /></LayoutAdmin></PrivateRoute>} />
-        <Route path="/Administrador/configuracion" element={<PrivateRoute><LayoutAdmin><Configuracion /></LayoutAdmin></PrivateRoute>} />
-        <Route path="/Administrador/reportes" element={<PrivateRoute><LayoutAdmin><Reportes /></LayoutAdmin></PrivateRoute>} />
-        <Route path="/Administrador/pacientes" element={<PrivateRoute><LayoutAdmin><Pacientes /></LayoutAdmin></PrivateRoute>} />
-        <Route path="/Administrador/PerfilEmpresa" element={<PrivateRoute><LayoutAdmin><PerfilEmpresa /></LayoutAdmin></PrivateRoute>} />
+        <Route
+          path="/Administrador/principal"
+          element={
+            <PrivateRoute>
+              <LayoutAdmin>
+                <PrincipalAdmin />
+              </LayoutAdmin>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/Administrador/configuracion"
+          element={
+            <PrivateRoute>
+              <LayoutAdmin>
+                <Configuracion />
+              </LayoutAdmin>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/Administrador/reportes"
+          element={
+            <PrivateRoute>
+              <LayoutAdmin>
+                <Reportes />
+              </LayoutAdmin>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/Administrador/pacientes"
+          element={
+            <PrivateRoute>
+              <LayoutAdmin>
+                <Pacientes />
+              </LayoutAdmin>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/Administrador/PerfilEmpresa"
+          element={
+            <PrivateRoute>
+              <LayoutAdmin>
+                <PerfilEmpresa />
+              </LayoutAdmin>
+            </PrivateRoute>
+          }
+        />
 
         {/* Ruta para manejo de errores */}
         <Route path="/error" element={<ErrorPage />} />
-        <Route path="*" element={<ErrorPage errorCode={404} errorMessage="Página no encontrada" />} />
+        <Route
+          path="*"
+          element={
+            <ErrorPage errorCode={404} errorMessage="Página no encontrada" />
+          }
+        />
       </Routes>
     </Router>
   );
--- src/components/Tools/ErrorPage.jsx
+++ src/components/Tools/ErrorPage.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import { 
-  Box, 
-  Typography, 
-  Button, 
-  Container, 
+import React, { useEffect, useState } from 'react';
+import {
+  Box,
+  Typography,
+  Button,
+  Container,
   Paper,
   useTheme,
   SvgIcon
 } from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import { Refresh, Home } from "@mui/icons-material";
 
 // Dental Tooth Icon component
 const ToothIcon = (props) => (
   <SvgIcon {...props} viewBox="0 0 512 512" sx={{ width: '120px', height: '120px' }}>
-    <path d="M256 0c-62.4 0-115.4 21.9-157.6 64C56.2 106.1 34.3 159.1 34.3 221.5c0 35.2 5.3 66.8 15.8 94.9 10.5 28.1 23.8 52.5 39.7 73.2 15.9 20.7 32.2 39.5 48.9 56.3 16.7 16.8 31.8 33.5 45.3 50 13.5 16.5 24 34.4 31.5 53.6 2.9 7.5 6.5 11.2 10.7 11.2 4.2 0 7.7-3.7 10.7-11.2 7.5-19.2 18-37 31.5-53.6 13.5-16.5 28.6-33.2 45.3-50 16.7-16.8 33-35.6 48.9-56.3 15.9-20.7 29.2-45.1 39.7-73.2 10.5-28.1 15.8-59.7 15.8-94.9 0-62.4-21.9-115.4-64-157.6C371.4 21.9 318.4 0 256 0zm0 384c-44.2 0-81.6-15.6-112.2-46.2C113.2 307.2 97.6 269.8 97.6 225.6c0-44.2 15.6-81.6 46.2-112.2C174.4 82.8 211.8 67.2 256 67.2c44.2 0 81.6 15.6 112.2 46.2 30.6 30.6 46.2 68 46.2 112.2 0 44.2-15.6 81.6-46.2 112.2C337.6 368.4 300.2 384 256 384z"/>
+    <path d="M256 0c-62.4 0-115.4 21.9-157.6 64C56.2 106.1 34.3 159.1 34.3 221.5c0 35.2 5.3 66.8 15.8 94.9 10.5 28.1 23.8 52.5 39.7 73.2 15.9 20.7 32.2 39.5 48.9 56.3 16.7 16.8 31.8 33.5 45.3 50 13.5 16.5 24 34.4 31.5 53.6 2.9 7.5 6.5 11.2 10.7 11.2 4.2 0 7.7-3.7 10.7-11.2 7.5-19.2 18-37 31.5-53.6 13.5-16.5 28.6-33.2 45.3-50 16.7-16.8 33-35.6 48.9-56.3 15.9-20.7 29.2-45.1 39.7-73.2 10.5-28.1 15.8-59.7 15.8-94.9 0-62.4-21.9-115.4-64-157.6C371.4 21.9 318.4 0 256 0zm0 384c-44.2 0-81.6-15.6-112.2-46.2C113.2 307.2 97.6 269.8 97.6 225.6c0-44.2 15.6-81.6 46.2-112.2C174.4 82.8 211.8 67.2 256 67.2c44.2 0 81.6 15.6 112.2 46.2 30.6 30.6 46.2 68 46.2 112.2 0 44.2-15.6 81.6-46.2 112.2C337.6 368.4 300.2 384 256 384z" />
   </SvgIcon>
 );
 
@@ -43,19 +44,38 @@ const errorTypes = {
     title: 'Error del Servidor',
     description: 'Ocurrió un error interno en el servidor. Por favor, inténtalo más tarde.',
     color: '#795548' // Marrón para errores del servidor
+  },
+  502: {
+    title: 'Error de Conexión',
+    description: 'No se pudo establecer conexión con el servidor. La página se actualizará automáticamente cuando se restablezca la conexión.',
+    color: '#F44336' // Usando rojo para indicar un problema crítico de conexión
   }
 };
 
 const ErrorPage = () => {
   const theme = useTheme();
   const location = useLocation();
-  const { errorCode = 404, errorMessage } = location.state || {};
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+  // Determinar el error
+  const errorCode = !isOnline ? 502 : location.state?.errorCode || 404;
+  const errorMessage = location.state?.errorMessage || errorTypes[errorCode].description;
   const errorInfo = errorTypes[errorCode] || errorTypes[404];
+  useEffect(() => {
+    const handleOnline = () => window.location.reload();
+    const handleOffline = () => setIsOnline(false);
 
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
   const containerVariants = {
     initial: { opacity: 0, y: 20 },
-    animate: { 
-      opacity: 1, 
+    animate: {
+      opacity: 1,
       y: 0,
       transition: { duration: 0.6 }
     }
@@ -63,10 +83,10 @@ const ErrorPage = () => {
 
   const numberVariants = {
     initial: { scale: 0.8, opacity: 0 },
-    animate: { 
+    animate: {
       scale: 1,
       opacity: 1,
-      transition: { 
+      transition: {
         duration: 0.8,
         ease: "easeOut"
       }
@@ -75,7 +95,7 @@ const ErrorPage = () => {
 
   const iconVariants = {
     initial: { rotate: 0 },
-    animate: { 
+    animate: {
       rotate: 360,
       transition: {
         duration: 20,
@@ -147,8 +167,8 @@ const ErrorPage = () => {
 
           {/* Error Content */}
           <motion.div variants={numberVariants}>
-            <Typography 
-              variant="h1" 
+            <Typography
+              variant="h1"
               sx={{
                 fontSize: { xs: '6rem', sm: '8rem', md: '10rem' },
                 fontWeight: 900,
@@ -161,9 +181,9 @@ const ErrorPage = () => {
             </Typography>
           </motion.div>
 
-          <Typography 
-            variant="h4" 
-            sx={{ 
+          <Typography
+            variant="h4"
+            sx={{
               mb: 1,
               color: 'text.primary',
               fontWeight: 700
@@ -172,40 +192,57 @@ const ErrorPage = () => {
             {errorInfo.title}
           </Typography>
 
-          <Typography 
-            variant="h6" 
-            sx={{ 
+          <Typography
+            variant="h6"
+            sx={{
               mb: 4,
               color: 'text.secondary',
               maxWidth: '600px',
               mx: 'auto'
             }}
           >
-            {errorMessage ||errorInfo.description}
+            {errorMessage || errorInfo.description}
           </Typography>
 
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Button
-              component={Link}
-              to="/"
-              variant="contained"
-              sx={{
-                bgcolor: errorInfo.color,
-                color: 'white',
-                px: 4,
-                py: 1.5,
-                borderRadius: 2,
-                fontSize: '1.1rem',
-                transition: 'all 0.3s ease',
-                '&:hover': {
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            {errorCode === 502 ? (
+              <Button
+                variant="contained"
+                onClick={() => window.location.reload()}
+                sx={{
                   bgcolor: errorInfo.color,
-                  filter: 'brightness(90%)',
-                  transform: 'scale(1.05)'
-                }
-              }}
-            >
-              Volver al Inicio
-            </Button>
+                  color: "white",
+                  px: 3,
+                  py: 1.2,
+                  borderRadius: 2,
+                  fontSize: "1.5rem",
+                  transition: "all 0.3s ease",
+                  "&:hover": { filter: "brightness(90%)", transform: "scale(1.05)" },
+                  minWidth: "60px", // Asegurar que el botón no sea muy pequeño
+                }}
+              >
+                <Refresh fontSize="large" />
+              </Button>
+            ) : (
+              <Button
+                component={Link}
+                to="/"
+                variant="contained"
+                sx={{
+                  bgcolor: errorInfo.color,
+                  color: "white",
+                  px: 3,
+                  py: 1.2,
+                  borderRadius: 2,
+                  fontSize: "1.5rem",
+                  transition: "all 0.3s ease",
+                  "&:hover": { filter: "brightness(90%)", transform: "scale(1.05)" },
+                  minWidth: "60px",
+                }}
+              >
+                <Home fontSize="large" />
+              </Button>
+            )}
           </Box>
         </Paper>
       </Box>
